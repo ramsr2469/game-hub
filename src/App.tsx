739f9436ac8976bb7cc7e9a95949b474269186a9
ramesh ramavaram <ramesh.ramavaram@walmart.com>
2024-04-25 21:46:30 -0700

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 function App() {
   return (
     <Grid
@@ -7,15 +8,15 @@ function App() {
         lg: `"nav nav" "aside main"`,
       }}
     >
-      <GridItem area="nav" bg="coral">
-        Nav
+      <GridItem area="nav">
+        <NavBar />
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" bg="gold">
           aside
         </GridItem>
       </Show>
-      <GridItem area="main" bg="dodgeblue">
+      <GridItem area="main" bg="red">
         main
       </GridItem>
     </Grid>
